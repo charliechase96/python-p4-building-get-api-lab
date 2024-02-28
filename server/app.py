2f161409a54cf82013f2979da980ad84e679e99d
Charlie Gray <charliechasegray@gmail.com>
2024-02-27 23:22:24 -0700

    finish lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,23 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return make_response(jsonify([bakery.to_dict() for bakery in bakeries]))
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    return make_response(jsonify(bakery.to_dict()))
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return make_response(jsonify([good.to_dict() for good in baked_goods]))
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return make_response(jsonify(most_expensive_good.to_dict()))
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
